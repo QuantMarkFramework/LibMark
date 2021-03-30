21f0ffa0a29c5ae76ca90fdee326e9b4710cfe54
Becker Leo <leijonamieli@hotmail.fi>
2021-03-31 01:16:01 +0300

    Small fixes and tests for circuit module.

--- quantmark/circuit.py
+++ quantmark/circuit.py
@@ -1,4 +1,6 @@
 import re
+import typing
+from dataclasses import dataclass
 import tequila as tq
 from quantmark.exceptions.same_control_and_target import SameControlAndTarget
 from quantmark.exceptions.invalid_syntax_error import InvalidSyntaxError
@@ -7,7 +9,14 @@ from quantmark.create_multiline_regex import create_multiline_regex
 NP_ONEQ_GATES_REGEX = "(X|Y|Z|H)\(target=\(\d+,\)(, control=\((\d+,|\d+(, \d+)+)\))?\)"
 P_ONEQ_GATES_REGEX = "(Phase|Rx|Ry|Rz)\(target=\(\d+,\)(, control=\((\d+,|\d+(, \d+)+)\))?,"\
 	" parameter=((\d+.\d*)|\D*)\)"
-SWAP_GATE_REGEX = "SWAP\(target=\(\d*, \d*\)(, control=\((\d+,|\d+(, \d+)+)\))?\)"
+SWAP_GATE_REGEX = "SWAP\(target=\(\d*, \d*\)(, control=\((\d+,|\d+(, \d+)+|)\))?\)"
+
+@dataclass
+class GateDict:
+	name: str
+	target: typing.List[str]
+	control: typing.List[str] = None
+	parameter: typing.Union[str, float] = None
 
 def circuit_pattern():
 	options = [NP_ONEQ_GATES_REGEX, P_ONEQ_GATES_REGEX, SWAP_GATE_REGEX]
@@ -40,28 +49,22 @@ def gate_string_to_dict(string: str):
 	parameter = None
 	if 'parameter' in string:
 		parameter = get_gate_parameter(string)
-	return {
-		'name': name,
-		'target': target,
-		'control': control,
-		'parameter': parameter
-	}
+	return GateDict(name=name, target=target, control=control, parameter=parameter)
 
-def gate_from_dict(gate_dict: dict):
-	g = gate_dict
-	if set(g['target']) & set(g['control']):
+def gate_from_gate_dict(gate: GateDict):
+	if gate.control and set(gate.target) & set(gate.control):
 		raise SameControlAndTarget
-	if g['name'] in ['X', 'Y', 'Z', 'H']:
-		gate_method = getattr(tq.gates, g['name'])
-		return gate_method(target=g['target'], control=g['control'])
-	if g['name'] in ['Rx', 'Ry', 'Rz']:
-		gate_method = getattr(tq.gates, g['name'])
-		return gate_method(g['parameter'], target=g['target'], control=g['control'])
-	if g['name'] in ['Phase']:
-		return tq.gates.Phase(phi=g['parameter'], target=g['target'], control=g['control'])
-	if g['name'] in ['SWAP']:
-		first, second = g['target']
-		return tq.gates.SWAP(first=first, second=second, control=g['control'])
+	if gate.name in ['X', 'Y', 'Z', 'H']:
+		gate_method = getattr(tq.gates, gate.name)
+		return gate_method(target=gate.target, control=gate.control)
+	if gate.name in ['Rx', 'Ry', 'Rz']:
+		gate_method = getattr(tq.gates, gate.name)
+		return gate_method(gate.parameter, target=gate.target, control=gate.control)
+	if gate.name in ['Phase']:
+		return tq.gates.Phase(phi=gate.parameter, target=gate.target, control=gate.control)
+	if gate.name in ['SWAP']:
+		first, second = gate.target
+		return tq.gates.SWAP(first=first, second=second, control=gate.control)
 	return None
 
 def circuit_from_string(circuit: str):
@@ -74,7 +77,7 @@ def circuit_from_string(circuit: str):
 	gates = [gate_string_to_dict(g[0]) for g in gates]
 	if not gates:
 		return None
-	circuit = gate_from_dict(gates[0])
+	circuit = gate_from_gate_dict(gates[0])
 	for gate in gates[1:]:
-		circuit += gate_from_dict(gate)
+		circuit += gate_from_gate_dict(gate)
 	return circuit
