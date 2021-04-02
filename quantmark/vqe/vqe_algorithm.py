3e58c27336890a46fbc8b99b8ab08923730f307b
sovu <sonja.vuorinen@helsinki.fi>
2021-04-02 17:05:04 +0300

    VQE algorithm getters and setters

--- quantmark/vqe/vqe_algorithm.py
+++ quantmark/vqe/vqe_algorithm.py
@@ -5,6 +5,7 @@ from tequila import QubitHamiltonian
 from quantmark.qm_backend import QMBackend as Backend
 from quantmark.qm_optimizer import QMOptimizer as Optimizer
 from quantmark.vqe.vqe_result import VQEResult as Result
+from quantmark.circuit import circuit_from_string
 
 class VQEAlgorithm:
 	def __init__(
@@ -19,9 +20,9 @@ class VQEAlgorithm:
 		target_value: int = None 
 		):
 		if not molecule and not hamiltonian:
-			raise Exception('You have give to a molecule or a hamiltonian')
+			raise Exception('You have give to a molecule or a hamiltonian.')
 		if molecule and hamiltonian:
-			raise Exception('You have give to a molecule or a hamiltonina not both.')
+			raise Exception('You have give to a molecule or a hamiltonian not both.')
 		self._circuit = circuit
 		self._optimizer = optimizer
 		self._backend = backend
@@ -31,11 +32,81 @@ class VQEAlgorithm:
 		self._repetitions = repetitions
 		self._target_value = target_value
 
+	@property
+	def circuit(self):
+		return self._circuit
+
+	@circuit.setter
+	def circuit(self, circuit):
+		if isinstance(circuit, str):
+			circuit = circuit_from_string(circuit)
+		self._circuit = circuit
+
+	@property
+	def optimizer(self):
+		return self._optimizer
+
+	@optimizer.setter
+	def optimizer(self, optimizer):
+		self._optimizer = optimizer
+	
+	@property
+	def molecule(self):
+		return self._molecule
+
+	@molecule.setter
+	def molecule(self, molecule):
+		if self._hamiltonian:
+			raise Exception('Cannot set molecule when there is a hamiltonian.')
+		self._molecule = molecule
+	
+	@property
+	def hamiltonian(self):
+		return self._hamiltonian
+
+	@hamiltonian.setter
+	def hamiltonian(self, hamiltonian):
+		if self._molecule:
+			raise Exception('Cannot set hamiltonian when there is a molecule.')
+		self._hamiltonian = hamiltonian
+
+	@property
+	def backend(self):
+		return self._backend
+
+	@backend.setter
+	def backend(self, backend):
+		self._backend = backend
+
+	@property
+	def silent(self):
+		return self._silent
+
+	@silent.setter
+	def silent(self, silent):
+		self._silent = silent
+	
+	@property
+	def repetitions(self):
+		return self._repetitions
+
+	@repetitions.setter
+	def repetitions(self, repetitions):
+		self._repetitions = repetitions
+	
+	@property
+	def target_value(self):
+		return self._target_value
+
+	@target_value.setter
+	def target_value(self, target_value):
+		self._target_value = target_value
+
 	def analyze(self):
 		if not self._molecule and not self._hamiltonian:
-			raise Exception('You have give to a molecule or a hamiltonina')
+			raise Exception('You have give to a molecule or a hamiltonian.')
 		if self._molecule and self._hamiltonian:
-			raise Exception('You have give to a molecule or a hamiltonina not both.')
+			raise Exception('You have give to a molecule or a hamiltonian not both.')
 		if self._molecule:
 			objective = tq.ExpectationValue(H=self._molecule.make_hamiltonian(), U=self._circuit)
 		if self._hamiltonian:
