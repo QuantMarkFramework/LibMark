8cc729ad72d2aa8b44f36bdcaf8ddcefead5a740
Henri0088 <Henri.Sundquist@rokki.net>
2021-06-07 21:51:18 +0300

    Add docstrings and fix style errors

--- QuantMark/result_sender.py
+++ QuantMark/result_sender.py
@@ -2,9 +2,29 @@ import requests
 
 # Use this (or wherever your local WebMark2 is running) while developing
 url = 'http://0.0.0.0:8000/api/'
-#url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
+# url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
+
 
 class Qresult:
+    """
+    A class to capture data from a VQE run.
+    ---
+    Attributes
+    ----------
+    results : str[]
+        results returned by tq.minimize in str form
+    molecules : str[]
+        tequila molecule objects in str form
+    hamiltonian : str[]
+        hamiltonians in str form
+    ansatz : str[]
+        ansatz in str form
+
+    Methods
+    -------
+    add_run(result, molecule, hamiltonian, ansatz)
+    push()
+    """
     def __init__(self, optimizer):
         self.results = []
         self.molecules = []
@@ -13,17 +33,31 @@ class Qresult:
         self.optimizer = optimizer
 
     def add_run(self, result, molecule, hamiltonian, ansatz):
-        self.results.append(result)
+        """Add VQE run to the QResult
+
+        Parameters
+        ----------
+        result : tequila.optimizers.optimizer_scipy.SciPyResults
+            object returned by tq.minimize
+        molecule : tequila.quantumchemistry.psi4_interface.QuantumChemistryPsi4
+            molecule used in the run
+        hamiltonian : tequila.hamiltonian.qubit_hamiltonian.QubitHamiltonian
+            object returned by molecule.make_hamiltonian()
+        ansatz : tequila.circuit.circuit.QCircuit
+            object returned by molecule.make_uccsd_ansatz()
+        """
+        self.results.append(str(result))
         self.molecules.append(str(molecule))
-        self.hamiltonian.append(hamiltonian)
-        self.ansatz.append(ansatz)
+        self.hamiltonian.append(str(hamiltonian))
+        self.ansatz.append(str(ansatz))
 
     def push(self):
-        result_json = {"result":str(self.results),
-            "hamiltonian":str(self.hamiltonian),
-            "ansatz":str(self.ansatz),
-            "optimizer":str(self.optimizer),
-            "molecule":str(self.molecules)}
+        """Send Qresult to server"""
+        result_json = {"result": self.results,
+                       "hamiltonian": self.hamiltonian,
+                       "ansatz": self.ansatz,
+                       "optimizer": self.optimizer,
+                       "molecule": self.molecules}
         response = requests.post(url, data=result_json)
         return response
 
@@ -32,4 +66,4 @@ class Qresult:
             {len(self.hamiltonian)} hamiltonians,
             {len(self.ansatz)} ansatz,
             {len(self.molecules)} molecules,
-            with optimizer: {self.optimizer}""" 
+            with optimizer: {self.optimizer}"""
