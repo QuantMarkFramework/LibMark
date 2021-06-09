0ad6cf82d4356d06f5519608a37bb1d13a59332f
Henri0088 <Henri.Sundquist@rokki.net>
2021-06-09 12:24:35 +0300

    broke 'results' field down to smaller bits

--- QuantMark/result_sender.py
+++ QuantMark/result_sender.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # Use this (or wherever your local WebMark2 is running) while developing
 url = 'http://0.0.0.0:8000/api/'
@@ -26,7 +27,10 @@ class Qresult:
     push()
     """
     def __init__(self, optimizer):
-        self.results = []
+        self.scipy_results = []
+        self.energies = []
+        self.variables = []
+        self.histories = []
         self.molecules = []
         self.hamiltonian = []
         self.ansatz = []
@@ -46,23 +50,31 @@ class Qresult:
         ansatz : tequila.circuit.circuit.QCircuit
             object returned by molecule.make_uccsd_ansatz()
         """
-        self.results.append(str(result))
+        # FIXME calling str(*something*) to make the data JSON serializable is lazy,
+        # need to find a more elegant way to do this.
+        self.energies.append(result.energy)
+        self.variables.append(str(result.variables))
+        self.histories.append(str(result.history))
+        self.scipy_results.append(str(result.scipy_result))
         self.molecules.append(str(molecule))
         self.hamiltonian.append(str(hamiltonian))
         self.ansatz.append(str(ansatz))
 
     def push(self):
         """Send Qresult to server"""
-        result_json = {"result": self.results,
+        result = {"energies": self.energies,
+                       "variables": self.variables,
+                       "histories": self.histories,
+                       "scipy_results": self.scipy_results,
                        "hamiltonian": self.hamiltonian,
                        "ansatz": self.ansatz,
                        "optimizer": self.optimizer,
                        "molecule": self.molecules}
-        response = requests.post(url, data=result_json)
+        response = requests.post(url, json=json.dumps(result, indent=4))
         return response
 
     def __str__(self):
-        return f"""Collected: {len(self.results)} results,
+        return f"""Collected: {len(self.energies)} results,
             {len(self.hamiltonian)} hamiltonians,
             {len(self.ansatz)} ansatz,
             {len(self.molecules)} molecules,
