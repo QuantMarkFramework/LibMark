bfd69a23cd3a086a8382fece582082018fa5e227
Henri0088 <Henri.Sundquist@rokki.net>
2021-06-17 21:14:28 +0300

    Made data collection more general and updated readme picture

--- quantmark/result_sender.py
+++ quantmark/result_sender.py
@@ -3,30 +3,12 @@ import json
 import tequila
 
 # Use this (or wherever your local WebMark2 is running) while developing
-# url = 'http://0.0.0.0:8000/api/'
-url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
+url = 'http://0.0.0.0:8000/api/'
+# url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
 
 
 class Qresult:
-    """
-    A class to capture data from a VQE run.
-    ---
-    Attributes
-    ----------
-    results : str[]
-        results returned by tq.minimize in str form
-    molecules : str[]
-        tequila molecule objects in str form
-    hamiltonian : str[]
-        hamiltonians in str form
-    ansatz : str[]
-        ansatz in str form
 
-    Methods
-    -------
-    add_run(result, molecule, hamiltonian, ansatz)
-    push()
-    """
     def __init__(self, optimizer):
         self.scipy_results = []
         self.energies = []
@@ -40,26 +22,13 @@ class Qresult:
         self.distances = []
 
     def make_history_dict(self, history):
-        h_dict = {}
-        h_dict['history_energies'] = history.energies
-        h_dict['gradients'] = history.gradients
-        h_dict['angles'] = str(history.angles)
-        h_dict['energies_calls'] = history.energies_calls
-        h_dict['gradients_calls'] = history.gradients_calls
-        h_dict['angles_calls'] = history.angles_calls
-        return h_dict
+        return history.__dict__
 
     def make_scipy_result_dict(self, scipy_result):
-        sr_dict = {}
-        sr_dict['final_simplex'] = str(scipy_result.final_simplex)
-        sr_dict['fun'] = scipy_result.fun
-        sr_dict['message'] = str(scipy_result.message)
-        sr_dict['nfev'] = scipy_result.nfev
-        sr_dict['nit'] = scipy_result.nit
-        sr_dict['status'] = scipy_result.status
-        sr_dict['success'] = str(scipy_result.success)
-        sr_dict['x'] = str(scipy_result.x)
-        return sr_dict
+        scipy_result_dict = {}
+        for key in scipy_result.keys():
+            scipy_result_dict[key] = str(scipy_result.get(key))
+        return scipy_result_dict
 
     def add_run(self, result, molecule, hamiltonian, ansatz):
         """Add VQE run to the QResult
@@ -79,10 +48,10 @@ class Qresult:
         # is lazy, need to find a more elegant way to do this.
         self.energies.append(result.energy)
         self.variables.append(str(result.variables).replace('\n', ' '))
-        self.histories.append(str(self.make_history_dict(result.history)))
-        self.scipy_results.append(
-            str(self.make_scipy_result_dict(result.scipy_result))
-            )
+        self.histories.append(str(result.history.__dict__))
+        self.scipy_results.append(str(
+            self.make_scipy_result_dict(result.scipy_result)
+        ))
         self.molecules.append(str(molecule))
         self.hamiltonian.append(str(hamiltonian))
         self.ansatz.append(str(ansatz))
