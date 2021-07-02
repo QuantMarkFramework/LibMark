a396b7b6352f08e841bbcdc046b2ae1c69dd1e2a
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-07-02 21:22:55 +0300

    Tidy code

--- push.py
+++ push.py
@@ -5,7 +5,7 @@ import requests
 
 url = 'http://0.0.0.0:8000/api/'
 # url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
-print(str(sys.argv))
+
 if len(sys.argv) == 2 and len(sys.argv[1]):
     file = open(sys.argv[1], "r")
     struct = file.read()
