c9c9c8bf91027f33348ae6a0355a650718c63ad6
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-07-02 21:22:20 +0300

    Add push.py for pushing JSON to server

---
+++ push.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+import requests
+
+url = 'http://0.0.0.0:8000/api/'
+# url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
+print(str(sys.argv))
+if len(sys.argv) == 2 and len(sys.argv[1]):
+    file = open(sys.argv[1], "r")
+    struct = file.read()
+    response = requests.post(url, json=struct)
+    print(response)
+else:
+    print("You forgot the file!")
