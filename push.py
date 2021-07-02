12156b493f27bf7cb8c79a65700dc73ceb8ee773
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-07-02 21:28:42 +0300

    Simplified argument verification

--- push.py
+++ push.py
@@ -6,7 +6,7 @@ import requests
 url = 'http://0.0.0.0:8000/api/'
 # url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
 
-if len(sys.argv) == 2 and len(sys.argv[1]):
+if len(sys.argv) == 2:
     file = open(sys.argv[1], "r")
     struct = file.read()
     response = requests.post(url, json=struct)
