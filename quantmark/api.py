07c4d2edd8964f056a9ee2d4a9b58e16d30b8c35
Henri Sundquist <henri.sundquist@rokki.net>
2021-08-05 21:27:19 +0300

    Add support for downloading all info about a result as a large dictionary

--- quantmark/api.py
+++ quantmark/api.py
@@ -2,8 +2,8 @@ import requests
 import ast
 
 # Use this (or wherever your local WebMark2 is running) while developing
-# url = 'http://localhost:8000/api/'
-url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
+url = 'http://localhost:8000/api/'
+# url = 'https://ohtup-staging.cs.helsinki.fi/qleader/api/'
 
 
 def get_distances():
@@ -12,4 +12,13 @@ def get_distances():
     return list
     """
     res = requests.get(f'{url}distances/')
+    return ast.literal_eval(res.content.decode('UTF-8'))
+
+
+def get_experiment(id, token):
+    """Get a dump of all data available about an experiment"""
+    headers = {
+            'Authorization': f'Token {token}'
+        }
+    res = requests.get(f'{url}{id}/download', headers=headers)
     return ast.literal_eval(res.content.decode('UTF-8'))
